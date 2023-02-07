205177e26a932aa4fd58c93ef865afbea7836fd8
Chris Korbel <ckorbel@ckorbel-mn1.linkedin.biz>
2023-02-07 13:14:51 -0800

    move panels around

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -44,7 +44,7 @@ function Dashboard(): React.ReactNode {
 
   const { data, error, isLoading } = useQuery(
     ["players", "QB"],
-    () => fetchDraftPlayers("QB"),
+    () => fetchDraftPlayers("DE"),
     { staleTime: Infinity } // draft data should be unchanged
   );
 
@@ -179,8 +179,8 @@ function Dashboard(): React.ReactNode {
 
             {/* ROW 2 */}
             <Box
-              gridColumn="span 8"
-              gridRow="span 2"
+              gridColumn="span 12"
+              gridRow="span 3"
               sx={{
                 backgroundColor: colors.primary[400],
               }}
@@ -216,8 +216,58 @@ function Dashboard(): React.ReactNode {
                   </IconButton>
                 </Box>
               </Box>
-              <Box height="250px" m="-20px 0 0 0">
+              <Box height="400px" m="-20px 0 0 0">
                 <LineChart isDashboard={true} />
+              </Box>
+            </Box>
+
+            {/* ROW 3 */}
+            <Box
+              gridColumn="span 4"
+              gridRow="span 2"
+              p="30px"
+              sx={{
+                backgroundColor: colors.primary[400],
+              }}
+            >
+              <Typography variant="h5" fontWeight="600">
+                Campaign
+              </Typography>
+              <Box
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                mt="25px"
+              >
+                <ProgressCircle size="125" />
+                <Typography
+                  variant="h5"
+                  color={colors.greenAccent[500]}
+                  sx={{ mt: "15px" }}
+                >
+                  $48,352 revenue generated
+                </Typography>
+                <Typography>
+                  Includes extra misc expenditures and costs
+                </Typography>
+              </Box>
+            </Box>
+            <Box
+              gridColumn="span 4"
+              gridRow="span 2"
+              sx={{
+                backgroundColor: colors.primary[400],
+              }}
+            >
+              <Typography
+                variant="h5"
+                fontWeight="600"
+                sx={{ padding: "30px 30px 0 30px" }}
+              >
+                Sales Quantity
+              </Typography>
+              <Box height="250px" mt="-20px">
+                <BarChart isDashboard={true} />
               </Box>
             </Box>
             <Box
@@ -276,111 +326,11 @@ function Dashboard(): React.ReactNode {
                         backgroundColor: colors.greenAccent[500],
                       }}
                     >
-                      {percentageOfTotal}%
+                      {Math.floor((percentageOfTotal / 1) * 100)}%
                     </Box>
                   </Box>
                 )
               )}
-              {mockTransactions.map((transaction, i) => (
-                <Box
-                  key={`${transaction.txId}-${i}`}
-                  display="flex"
-                  justifyContent="space-between"
-                  alignItems="center"
-                  borderBottom={`4px solid ${colors.primary[500]}`}
-                  p="15px"
-                >
-                  <Box>
-                    <Typography
-                      color={colors.greenAccent[500]}
-                      variant="h5"
-                      fontWeight="600"
-                    >
-                      {transaction.txId}
-                    </Typography>
-                    <Typography color={colors.grey[100]}>
-                      {transaction.user}
-                    </Typography>
-                  </Box>
-                  <Box color={colors.grey[100]}>{transaction.date}</Box>
-                  <Box
-                    backgroundColor={colors.greenAccent[500]}
-                    p="5px 10px"
-                    borderRadius="4px"
-                  >
-                    ${transaction.cost}
-                  </Box>
-                </Box>
-              ))}
-            </Box>
-
-            {/* ROW 3 */}
-            <Box
-              gridColumn="span 4"
-              gridRow="span 2"
-              p="30px"
-              sx={{
-                backgroundColor: colors.primary[400],
-              }}
-            >
-              <Typography variant="h5" fontWeight="600">
-                Campaign
-              </Typography>
-              <Box
-                display="flex"
-                flexDirection="column"
-                alignItems="center"
-                mt="25px"
-              >
-                <ProgressCircle size="125" />
-                <Typography
-                  variant="h5"
-                  color={colors.greenAccent[500]}
-                  sx={{ mt: "15px" }}
-                >
-                  $48,352 revenue generated
-                </Typography>
-                <Typography>
-                  Includes extra misc expenditures and costs
-                </Typography>
-              </Box>
-            </Box>
-            <Box
-              gridColumn="span 4"
-              gridRow="span 2"
-              sx={{
-                backgroundColor: colors.primary[400],
-              }}
-            >
-              <Typography
-                variant="h5"
-                fontWeight="600"
-                sx={{ padding: "30px 30px 0 30px" }}
-              >
-                Sales Quantity
-              </Typography>
-              <Box height="250px" mt="-20px">
-                <BarChart isDashboard={true} />
-              </Box>
-            </Box>
-            <Box
-              gridColumn="span 4"
-              gridRow="span 2"
-              padding="30px"
-              sx={{
-                backgroundColor: colors.primary[400],
-              }}
-            >
-              <Typography
-                variant="h5"
-                fontWeight="600"
-                sx={{ marginBottom: "15px" }}
-              >
-                Geography Based Traffic
-              </Typography>
-              <Box height="200px">
-                <GeographyChart isDashboard={true} />
-              </Box>
             </Box>
           </Box>
         </Box>
